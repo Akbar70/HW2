4e5f8491eaa21ba0bcbe0594ba99f027e73be3c8
Akbar <sulajmankulovakbar@gmail.com>
2023-08-23 16:20:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// 1) треугольник в цикле
+// var triangle = "*";
+// while (triangle.length<=7){
+// 	console.log(triangle);
+// 	triangle+="*";
+// }
+
+
+// 2) FizzBuzz
+// for (var j = 1; j <= 101; j++){
+//     if (j % 15 === 0)
+//     console.log("FillBuzz");
+//     else if (j % 3 === 0)
+//     console.log("fizz");
+//     else if (j % 5 === 0)
+//     console.log("buzz");
+// else console.log(j);
+// }
+
+
+// 3) шахматная доска
+// var slash = '*';
+// var space = ' ';
+// var result = '';
+// var row = true;
+// for (var i = 0; i < 8; i++) {
+// for (var j = 0; j < 8; j++) {
+//     if ((j+row) % 2 == 0) {
+//     result += space;
+//     } else {
+//     result += slash;
+//     }
+// }
+// console.log(result);
+// result='';
+// row = ! row;
+// }
+
+// 1) допол
+// const numbers = ['Алекс' , 'Анна', 'Олег']
+// numbers.push(1,2,3)
+// console.log(numbers );
